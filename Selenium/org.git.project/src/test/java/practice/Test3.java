a7ac1341d2d81edcee2a6f2fae6d8cca3a028e98
malathi60 <88128247+malathi60@users.noreply.github.com>
2021-08-12 20:27:18 +0530

    Update Test3.java

--- Selenium/org.git.project/src/test/java/practice/Test3.java
+++ Selenium/org.git.project/src/test/java/practice/Test3.java
@@ -5,7 +5,7 @@ public class Test3 {
 	public static void main(String[] args) 
 	{
 			System.out.println("SWD jar used to automate webpages, \n Maven is a bulid managment tool, \n GitHub is a SCM "
-					+ "source code managment tool and Jenkins is a CI/CT DevOps tools");
+					+ "source code managment SCM tool and Jenkins is a CI/CT DevOps tools");
 	}
 
 }
